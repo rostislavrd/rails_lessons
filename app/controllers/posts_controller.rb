d253f45a655364607542ca7c1c6d828ccdfe66fa
Rostislav Dyuryagin <rostislavrd@gmail.com>
2014-11-15 03:53:41 +0400

    initial commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,32 @@
 class PostsController < ActionController::Base
 
-	  def index
-	   # instance param
-	   @posts = Post.all
-	  end
+  def index
+    # instance param
+    @posts = Post.all
+  end
 
-	  def show
-	  end
+  def show
+  end
+          
+  def new
+    @post = Post.new
+  end
 
-	  private
-	  		def post
-	  			@post ||= Post.find(params[:id])
-	  		end
+  def create
+    puts params
+    #flash[:notise] = "Post was successflly created"
+    Post.create(post_params)
+    redirect_to posts_path, notice: "Post was successflly created"
+  end	  
 
-	  helper_method :post
+  private
+  def post
+    @post ||= Post.find(params[:id])
+  end
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  helper_method :post
 end
