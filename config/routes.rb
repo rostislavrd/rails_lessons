2f814eb11c4986ce6178980088048734e538241a
Rostislav Dyuryagin <rostislavrd@gmail.com>
2014-11-22 02:13:22 +0400

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'posts#index'
+  resources :posts do resources :comments end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -12,7 +13,6 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :posts, only: [:index, :show, :new, :create]
 
   # Example resource route with options:
   #   resources :products do
